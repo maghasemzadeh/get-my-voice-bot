b3611a6a0419c6cb97ee36f45cf1ad0e5d4bfd7a
aminst <aminst79@gmail.com>
2019-04-19 22:36:26 +0430

    start, help, about messages

--- source.py
+++ source.py
@@ -5,9 +5,11 @@ bot = telebot.TeleBot("895692273:AAGmqn6xVZShiS1l9vNXNcrf83iXRnL8BVk")
 def send_welcome(message):
     bot.reply_to(message, "اینجا پیام شروع خواهد بود")
 
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
-	bot.reply_to(message, "اینجا پیام کمک خواهد بود")
+    bot.reply_to(message, "اینجا پیام کمک خواهد بود")
+
 
 @bot.message_handler(commands=['about'])
 def send_help(message):
