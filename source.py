55c818a3c2dac730bd0808e0dce1994672860651
aminst <aminst79@gmail.com>
2019-04-20 02:49:45 +0430

    number inline function

--- source.py
+++ source.py
@@ -1,4 +1,6 @@
 import telebot
+import time
+
 
 bot = telebot.TeleBot("895692273:AAGmqn6xVZShiS1l9vNXNcrf83iXRnL8BVk")
 @bot.message_handler(commands=['start'])
@@ -15,6 +17,14 @@ def send_help(message):
 def send_help(message):
     bot.reply_to(message, "اینجا پیام معرفی خواهد بود")
 
+@bot.message_handler(commands=['number'])
+def send_voice_link(message):
+    number = message.text[len(message.text)-2:]
+    bot.reply_to(message, "the number is " + number)
 
-bot.polling()
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(6)
 
